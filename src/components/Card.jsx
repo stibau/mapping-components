76abc71cafd6d38b8c698fb85327a788f481260f
stibau <48186853+stibau@users.noreply.github.com>
2021-01-30 16:53:49 +0100

    Solution

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,7 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <Detail detailInfo={props.id} />
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
       </div>
